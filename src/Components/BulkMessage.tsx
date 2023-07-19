30f57a8fdd2bc56e241e60a66be399e73da6b3c2
Henrique Massareli <henrique@oneseventech.com>
2023-07-18 22:28:17 -0300

    feat: first commit

---
+++ src/Components/BulkMessage.tsx
@@ -0,0 +1,3 @@
+const BulkMessager = () => {
+  return <div></div>;
+};
---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,html,css,ts,jsx,tsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
